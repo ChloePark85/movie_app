f19aa22e803b37037a1c8ac848c44c790d4cb709
Hyunah Park <hapark85@gmail.com>
2021-01-16 22:59:18 +0900

    added map method

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import React from 'react';
 
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>;
-      <img src={picture} />
-    </div>
-  );
-}
 
 const foodILike = [{
+  id: 1,
   name:"Kimchi",
   image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
 },
 {
+  id: 2,
   name:"Doncasu",
   image:"https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
 },
 {
+  id: 3,
   name:"Kimbap",
   image:"http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
 }]
 
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <img src={picture} alt={name}/>
+    </div>
+  );
+}
+
+
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
-      {foodILike.map(dish  => <Food name={dish.name} picture= {dish.image}/>)}
+      {foodILike.map(dish  => (
+      <Food key={dish.id} name={dish.name} picture= {dish.image}/>
+      ))}
     </div>
   );
 }
