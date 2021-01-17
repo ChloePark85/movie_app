eefdefbb8a3544175642c179769d7911969d7798
Hyunah Park <hapark85@gmail.com>
2021-01-17 22:45:53 +0900

    fetched movie api

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
 
 
-const foodILike = [{
-  id: 1,
-  name:"Kimchi",
-  image: "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
-},
-{
-  id: 2,
-  name:"Doncasu",
-  image:"https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
-},
-{
-  id: 3,
-  name:"Kimbap",
-  image:"http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
-}]
-
-function Food({ name, picture }) {
-  return (
-    <div>
-      <h2>I like {name}</h2>
-      <img src={picture} alt={name}/>
-    </div>
-  );
-}
-
-
-function App() {
-  return (
-    <div>
-      {foodILike.map(dish  => (
-      <Food key={dish.id} name={dish.name} picture= {dish.image}/>
-      ))}
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {data: {data : {movies}}} = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    this.setState({movies, isLoading: false})
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading ? "Loading..." : movies.map(movie => {
+          console.log(movie);
+          return <Movie key = {movie.id} id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image}/>
+        } )}
+      </div>
+    );
+  }
 }
 
 export default App;
